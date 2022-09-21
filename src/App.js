b12e847cc66304ac53ba1ede3dfcaba80d5bce07
Hyperion04 <eliasmanca20@gmail.com>
2022-09-21 16:58:04 -0300

    Agrego componente Toolbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Toolbar } from './components/toolbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Toolbar />
     </div>
   );
 }
---
+++ src/components/toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FaFreeCodeCamp } from "react-icons/fa";
+
+export function Toolbar() {
+  return(
+    <div className='toolbar'>
+      <FaFreeCodeCamp />
+
+    </div>
+  )
+} 
