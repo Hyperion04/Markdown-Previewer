f19462f5c7023da2e386aa0ad99de48b88ab8786
Hyperion04 <eliasmanca20@gmail.com>
2022-09-21 17:37:12 -0300

    Añade toolbar.scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Toolbar } from './components/toolbar';
 
 function App() {
--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FaFreeCodeCamp } from "react-icons/fa";
+import '../stylesheets/toolbar.scss'
 
-export function Toolbar() {
+export function Toolbar(props) {
   return(
     <div className='toolbar'>
-      <FaFreeCodeCamp />
-
+      <FaFreeCodeCamp className='freecodecamp-logo'/>
+      {props.text}
+      <i className={props.icon} onClick={props.onClick} />
     </div>
   )
 } 
